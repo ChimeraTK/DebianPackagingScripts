e7cfed40ea17c6d0def2504ab12483a498fa4d9c
Martin Hierholzer <martin.hierholzer@desy.de>
2018-12-05 16:51:13 +0100

    make sure script also works when the working directories have been deleted

--- ppa/uploadPackageToPPA.sh
+++ ppa/uploadPackageToPPA.sh
@@ -33,30 +33,48 @@ echo "Working on package '${PACKAGE}'..."
 SourceURI=`grep "^SourceURI:" "DebianBuildVersions/${PACKAGE}/CONFIG" | sed -e 's/SourceURI: *//'`
 SourceBaseName="`basename "${SourceURI}"`"
 
-# Update source tree
-if [ ! -d "${SourceBaseName}/.git" ]; then
-  git clone ${SourceURI}
-else
-  cd "${SourceBaseName}"
-  git fetch
-  cd ..
-fi
-
 # Find latest build in DebianBuildVersions
 LAST_BUILD_FILE=`find "DebianBuildVersions/${PACKAGE}" -name LAST_BUILD | sort | tail -n1`
 LAST_BUILD_PATH=`cat "${LAST_BUILD_FILE}"`
 LAST_BUILD_NUMBER=`cat "DebianBuildVersions/${LAST_BUILD_PATH}/BUILD_NUMBER"`
+LAST_BUILD_DIR="`dirname ${LAST_BUILD_FILE}`/${LAST_BUILD_NUMBER}"
+SOURCE_PACKAGE=`grep "^Source:" "${LAST_BUILD_DIR}/control" | sed -e 's/^Source: *//'`
+
+# initialise bazaar working copy if not yet done
+mkdir -p "${SourceBaseName}"
+cd "${SourceBaseName}"
+if [ ! -d .bzr ]; then
+  bzr branch lp:~chimeratk/chimeratk/${SOURCE_PACKAGE}-package . --use-existing-dir || bzr init
+else
+  bzr merge lp:~chimeratk/chimeratk/${SOURCE_PACKAGE}-package
+fi
+cd ..
+
+# Update source tree
+if [ ! -d "${SourceBaseName}/.git" ]; then
+  echo "Downloading new source tree from git..."
+  cd "${SourceBaseName}"
+  git init .
+  git remote add origin "${SourceURI}"
+  git fetch
+  cd ..
+else
+  echo "Updating source tree from git..."
+  cd "${SourceBaseName}"
+  git fetch
+  cd ..
+fi
 
 # Determine the tag and check it out
 SOURCE_VERSION_NOPATCH=`echo "${LAST_BUILD_FILE}" | sed -e "s_^DebianBuildVersions/${PACKAGE}/__" -e 's_/.*$__'`
 cd "${SourceBaseName}"
 SOURCE_VERSION=`git tag | grep "^${SOURCE_VERSION_NOPATCH}" | sort | tail -n1`
 echo "Using source version $SOURCE_VERSION"
+git reset ${SOURCE_VERSION}
 git checkout ${SOURCE_VERSION}
 cd ..
 
 # Copy Debian control files from the build
-LAST_BUILD_DIR="`dirname ${LAST_BUILD_FILE}`/${LAST_BUILD_NUMBER}"
 mkdir -p "${SourceBaseName}/debian"
 cp -r "${LAST_BUILD_DIR}"/* "${SourceBaseName}/debian"
 
@@ -103,14 +121,9 @@ rename s/${REFERENCE_CODENAME}/ubuntu/ ${SourceBaseName}/debian/*.install
 # Hack the rules file to set the build version
 sed -i "${SourceBaseName}/debian/rules" -e 's,^#!/usr/bin/make -f$,#!/usr/bin/make -f\nexport PROJECT_BUILDVERSION=ubuntu'${LAST_BUILD_NUMBER}','
 
-# initialise bazaar working copy if not yet done
-cd "${SourceBaseName}"
-if [ ! -d .bzr ]; then
-  bzr init
-fi
-
 # check if anything modified. if not, we are done
-if [ `bzr status | wc -l` -eq 0 ]; then
+cd "${SourceBaseName}"
+if [ `bzr status | grep -v ".git" | grep -v "^modified:$" | grep -v "^unknown:$" | wc -l` -eq 0 ]; then
   echo "*** No changes, nothing to commit."
   exit 0
 fi
@@ -119,7 +132,6 @@ fi
 DEBIAN_VERSION=`echo ${SOURCE_VERSION} | sed -e 's/\(..\...\)/\1ubuntu'${LAST_BUILD_NUMBER}'/'`
 
 # generate changelog file
-SOURCE_PACKAGE=`grep "^Source:" "debian/control" | sed -e 's/^Source: *//'`
 rm -f debian/changelog
 debchange --create --package ${SOURCE_PACKAGE} -v ${DEBIAN_VERSION} "Automated preparation for the PPA"
 
