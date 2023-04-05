7786913ae90de33edd216e1b2f8171661bf4be20
d-rothe <dietrich.rothe@desy.de>
2023-04-05 13:09:50 +0200

    Fix: detect libdir (#34)
    * automatically detect used libdir
    
    from  debian/rules, detect_libdir.sh is run after build; install steps and and analyses result in order to find libdir used by package
    depending on package, libdir=/usr/lib or /usr/lib/x86_64-linux-gnu
    @LIBDIR@ in *.install files is then replaced
    
    * remove project name in *.install
    
    because it would need to be uppercase. Replace by wildcards.
    
    * check for pkgconfig instead of -config script
    
    ---------
    
    Co-authored-by: Martin Killenberg <martin.killenberg@desy.de>

---
+++ templates/detect_libdir.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+BUILDDIR=debian/tmp
+INSTFILEDIR=debian
+
+echo "detect_libdir: build dir contents"
+find ${BUILDDIR}
+
+# detect libdir used by package's build;install command.
+# It should be either /usr/lib or /usr/lib/x86_64-linux-gnu.
+# For non-lib packages, where neither is used in output, result variable LIBDIR will be empty
+LIBDIR=
+c=`find ${BUILDDIR} -wholename '*/usr/lib/x86_64-linux-gnu/*' -print -quit | wc -l`
+if [ $c -gt 0 ]; then
+    LIBDIR=/usr/lib/x86_64-linux-gnu
+else
+    c=`find ${BUILDDIR} -wholename '*/usr/lib/*' -print -quit | wc -l`
+    if [ $c -gt 0 ]; then
+        LIBDIR=/usr/lib
+    fi
+fi
+
+# replace @LIBDIR@ in install-lists
+for f in ${INSTFILEDIR}/*.install; do
+    sed -e "s|@LIBDIR@|${LIBDIR}|g" -i $f
+done
+
+
