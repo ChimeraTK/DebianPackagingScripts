f9b9851249836c6255ac65e2c16a11e94a34ae3e
Martin Hierholzer <martin.hierholzer@desy.de>
2024-10-14 09:44:11 +0200

    chore: update bash-completeion distribution code names

--- bash-completion.sh
+++ bash-completion.sh
@@ -7,14 +7,14 @@
 _debian_packaging_master_completions() {
   local suggestions=()
   nargs=$(( ${#COMP_WORDS[@]} - 1 ))
-  
+
   # first argument: distribution code name
   if [ ${nargs} -eq 1 ]; then
-    suggestions+=("xenial")
-    suggestions+=("bionic")
     suggestions+=("focal")
+    suggestions+=("noble")
+    suggestions+=("bookworm")
   fi
-  
+
   # even argument: package name
   if [ $(( nargs % 2 )) -eq 0 ]; then
     if [ -d DebianBuildVersions ]; then
@@ -27,12 +27,12 @@ _debian_packaging_master_completions() {
       cd ..
     fi
   fi
-  
+
   # odd argument (but not first): version: just offer "latest" for now
   if [ $nargs -gt 1 -a $(( nargs % 2 )) -eq 1 ]; then
     suggestions+=("latest")
   fi
-  
+
   # filter non-matching suggestions if something has already been typed
   start=${COMP_WORDS[$((${#COMP_WORDS[@]}-1))]}
   for s in "${suggestions[@]}"; do
@@ -40,7 +40,7 @@ _debian_packaging_master_completions() {
       COMPREPLY+=("$s")
     fi
   done
-  
+
 }
 
 complete -F _debian_packaging_master_completions ./master
