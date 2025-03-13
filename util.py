a40945fe473610a01fe906fe808c6bd58eeac5cd
Jens Georg <jens.georg@desy.de>
2025-03-13 18:10:47 +0100

    fixup! fix: Fix leaking Ubuntu packages into Debian build

---
+++ util.py
@@ -0,0 +1,31 @@
+# SPDX-FileCopyrightText: Deutsches Elektronen-Synchrotron DESY, MSK, ChimeraTK Project <chimeratk-support@desy.de>
+# SPDX-License-Identifier: LGPL-3.0-or-later
+
+import requests
+import zlib
+import lzma
+
+def downloadAndUnpack(url: str, localFile: str) -> bool:
+    print(f"Downloading {url}")
+    try:
+        with requests.get(url, stream=True) as request:
+            request.raise_for_status()
+            with open(localFile, 'wb') as f:
+                d = None
+                if url.endswith('.gz'):
+                    d = zlib.decompressobj(zlib.MAX_WBITS | 32)
+                elif url.endswith('.xz'):
+                    d = lzma.LZMADecompressor()
+                for chunk in request.iter_content(chunk_size=8192):
+                    if d:
+                        f.write(d.decompress(chunk))
+                    else:
+                        f.write(chunk)
+
+                # zlib needs flushing, xz doesn't
+                if d and hasattr(d, 'flush'):
+                    f.write(d.flush())
+        return True
+    except Exception as e:
+        print(f"Failed to download {url}: {e}")
+        return False
